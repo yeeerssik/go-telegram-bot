6d489cf49618f4a2cc737725a1be3df9d91453e0
Yers <58120353+yeeerssik@users.noreply.github.com>
2023-08-15 00:48:47 +0600

    Add TODO

--- events/telegram/commands.go
+++ events/telegram/commands.go
@@ -63,6 +63,7 @@ func (p *Processor) savePage(chatID int, pageURL string, username string) (err e
 	return nil
 }
 
+// TODO: Fix ErrNoSavedPages
 func (p *Processor) sendRandom(chatID int, username string) (err error) {
 	defer func() { err = e.WrapIfErr("can't do command: can't send random", err) }()
 
